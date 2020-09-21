7eb78729b6947f6b0a2c87f832be43aa70528841
Johan Guzman <jguzm022@ucr.edu>
2020-09-21 15:20:11 -0700

    Updates

---
+++ precache-manifest.0536681178e909a40ba2ee88b662af43.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb433c8b8344b5643fefd6bdc3fd94d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3f212f0e39c8c747d90",
+    "url": "/static/css/main.07e7cd83.chunk.css"
+  },
+  {
+    "revision": "5359985326eeb3926929",
+    "url": "/static/js/2.d9afbd1c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d9afbd1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f212f0e39c8c747d90",
+    "url": "/static/js/main.5e637d10.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "e1be13810c83e33a72e83d1767ce1e6d",
+    "url": "/static/media/Magnitie1.e1be1381.jpg"
+  },
+  {
+    "revision": "88fbc6f4f3a9260dbe4ab967919cea65",
+    "url": "/static/media/Magnitie2.88fbc6f4.jpg"
+  },
+  {
+    "revision": "8ca0b057c422f8a8fed7bcf1803a87d4",
+    "url": "/static/media/Magnitie3.8ca0b057.jpg"
+  },
+  {
+    "revision": "bc9ae60905fc53de0ad7b36ec7bbb047",
+    "url": "/static/media/Magnitie4.bc9ae609.jpg"
+  },
+  {
+    "revision": "5eed349e2ff65fa9b97a00a57410e0d7",
+    "url": "/static/media/Magnitie5.5eed349e.jpg"
+  },
+  {
+    "revision": "fff3b42b52e4e28d44a3fbd38f947172",
+    "url": "/static/media/MoodMixer1.fff3b42b.png"
+  },
+  {
+    "revision": "b1a0c4bdbafa6530ea498520bdc169e6",
+    "url": "/static/media/MoodMixer2.b1a0c4bd.png"
+  },
+  {
+    "revision": "6e07ef34e60499838dfc0e8e0230cf4b",
+    "url": "/static/media/MoodMixer3.6e07ef34.png"
+  },
+  {
+    "revision": "e2b5f2abfdfdee2477063874d5ee6870",
+    "url": "/static/media/TIMI1.e2b5f2ab.png"
+  },
+  {
+    "revision": "2a54e8e7a8553aa5809991dff16f96c9",
+    "url": "/static/media/TIMI2.2a54e8e7.png"
+  },
+  {
+    "revision": "ea1aa96ba352e0636e2ab6415f22dc4e",
+    "url": "/static/media/ThErapyScreening.ea1aa96b.jpg"
+  }
+]);
